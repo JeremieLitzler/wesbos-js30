c45a7d06d9009d1627e8f7bd229450f99d9c88a8
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-01-06 17:42:21 +0100

    exercice 19: save tasks 1, 2 and 4

--- exercices/19-Webcam.Fun/scripts.js
+++ exercices/19-Webcam.Fun/scripts.js
@@ -1,5 +1,86 @@
+var photoBooth = document.querySelector('.photobooth');
+
+if (!navigator.mediaDevices) {
+  var unsupported = document.createElement('p');
+  unsupported.style.fontSize = '2rem';
+  var message =
+    "Your browser doesn't support navigator.mediaDevices" +
+    '<br/>' +
+    'See <a href="https://developer.mozilla.org/en-US/docs/Web/API/Navigator/mediaDevices" target="_blank"> MDN documentation</a>.' +
+    '<br/>' +
+    'Use <a href="https://www.google.com/search?q=evergreen+browser" target="_blank">an evergreen browser!</a>';
+  unsupported.innerHTML = message;
+  document.body.appendChild(unsupported);
+  photoBooth.style.display = 'none';
+  throw message;
+}
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
+//where we put the photos taken
 const strip = document.querySelector('.strip');
+//the audio element
 const snap = document.querySelector('.snap');
+
+function handleWebcameError(err) {
+  const errorMsg = document.createElement('p');
+  errorMsg.className = 'error';
+  if (err.name.includes('NotAllowed')) {
+    errorMsg.innerHTML = 'You denied camera access...';
+  } else {
+    errorMsg.innerHTML = `Error ${err.name} occured!`;
+  }
+  document.body.appendChild(errorMsg);
+}
+
+function continuouslyPaintFrame() {
+  //take a frame every 16ms. Value depends on the machine...
+  //the higher the value, the less fluid it  will be.
+  //the lower the value, the more fluid it is but it can drain resources!
+  const refreshTime = 16;
+  const videoWidth = video.videoWidth;
+  const videoHeight = video.videoHeight;
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+  }, refreshTime);
+}
+
+function takePhoto() {
+  //Get the image as jpeg
+  //See https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+  //for the supported formats
+  const picture = canvas.toDataURL('image/jpeg');
+  const downloadableImage = document.createElement('a');
+  //Set the download URL
+  downloadableImage.href = picture;
+  //Set the image name
+  downloadableImage.setAttribute('download', 'image');
+  //add the image thumbnail
+  downloadableImage.innerHTML = `<img src="${picture}" alt="Video shot" />Image ${
+    strip.childNodes.length + 1
+  }`;
+  //Add downloadableImage to the list
+  strip.insertBefore(downloadableImage, strip.firstChild);
+}
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((mediaStream) => {
+      console.log(mediaStream);
+      video.srcObject = mediaStream;
+      video.load();
+      video.play();
+      console.dir(video);
+    })
+    .catch((err) => {
+      handleWebcamError(err);
+    });
+}
+
+getVideo();
+//Why listening to the event "canplay" rather than at the end of the .then() in the promise?
+//Because the Promise is run once!
+video.addEventListener('canplay', continuouslyPaintFrame);
--- notes/19-Webcam.Fun/Notes.md
+++ notes/19-Webcam.Fun/Notes.md
@@ -5,3 +5,29 @@
 [View next exercice's notes >](../20-Speech.Detection/Notes.md)
 
 ## Instructions
+
+We need to:
+
+- 1. get the webcam feed => ok
+- 2. continuously draw the last frame of the video as a photo in a canvas
+- 3. change the appearance of the frame in the canvas
+  - add a ["green screen" effect](https://www.google.com/search?q=green+screen+effect)
+- 4. be able to download the frame as an image from the canvas
+
+### Task 2
+
+How do we paint the last frame **continuously** in the canvas?
+
+- 1. How do we get the frame
+  - Found nothing in [this MDN](https://developer.mozilla.org/en-US/docs/Web/API/MediaStream)
+  - What does allow to run some code continuously in JavaScript? `setInterval`!
+- 2. How do we paint an image into the canvas?
+  - [MDN](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage) says the `image` parameter of `drawImage` can be an `HTMLVideoElement`.
+- 3. What size should the canvas be?
+  - The size the video captured: [height](https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/videoHeight) and [width](https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/videoWidth)
+
+### Task 4
+
+To get the image as jpeg, we can use [toDataURL](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL) of the canvas element.
+
+For the supported formats, check out MDN.
