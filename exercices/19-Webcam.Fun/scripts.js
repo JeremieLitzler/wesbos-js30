811eeedd224366adc69820b6a945c55b2e75d8b7
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-01-11 07:43:56 +0100

    exercice 19

--- exercices/19-Webcam.Fun/scripts.js
+++ exercices/19-Webcam.Fun/scripts.js
@@ -21,6 +21,10 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 //the audio element
 const snap = document.querySelector('.snap');
+let loggedPixels = false;
+let useRedEffect = false;
+let useRGBSplit = false;
+let useGreenScreen = false;
 
 function handleWebcameError(err) {
   const errorMsg = document.createElement('p');
@@ -33,6 +37,82 @@ function handleWebcameError(err) {
   document.body.appendChild(errorMsg);
 }
 
+function modifyPaintedFrame(videoWidth, videoHeight) {
+  if (loggedPixels) return;
+
+  let pixels = ctx.getImageData(0, 0, videoWidth, videoHeight);
+  //console.log(pixels);
+  //loggedPixels = true;
+  if (useRedEffect) redEffect(pixels);
+  if (useRGBSplit) rgbSplit(pixels);
+  if (useGreenScreen) greenScreen(pixels);
+}
+
+function enableRedEffect() {
+  useRedEffect = true;
+  useGreenScreen = useRGBSplit = !useRedEffect;
+}
+function redEffect(pixels) {
+  //we increment by 4 because we read one pixel at a time
+  for (let index = 0; index < pixels.data.length; index += 4) {
+    pixels.data[index] += 100; //red
+    pixels.data[index + 1] -= 50; //green
+    pixels.data[index + 2] *= 0.5; //blue
+  }
+  ctx.putImageData(pixels, 0, 0);
+}
+function enableRGBSplit() {
+  useRGBSplit = true;
+  useGreenScreen = useRedEffect = !useRGBSplit;
+}
+
+function rgbSplit(pixels) {
+  //we increment by 4 because we read one pixel at a time
+  for (let index = 0; index < pixels.data.length; index += 4) {
+    pixels.data[index + 50] = pixels.data[index]; //red
+    pixels.data[index - 300] = pixels.data[index + 1]; //green
+    pixels.data[index + 100] = pixels.data[index + 2]; //blue
+  }
+  ctx.putImageData(pixels, 0, 0);
+}
+
+function enableGreenScreen() {
+  useGreenScreen = true;
+  useRGBSplit = useRedEffect = !useGreenScreen;
+}
+
+function greenScreen(pixels) {
+  //levels are the range for each color for which we want to take
+  //the color out in the given image.
+  const levels = {};
+
+  //we read the levels from the inputs
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  //we increment by 4 because we read one pixel at a time
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    //so if the given color is within the range
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      //we will set the alpha to 0 = transparent.
+      pixels.data[i + 3] = 0;
+    }
+  }
+  ctx.putImageData(pixels, 0, 0);
+}
 function continuouslyPaintFrame() {
   //take a frame every 16ms. Value depends on the machine...
   //the higher the value, the less fluid it  will be.
@@ -44,6 +124,7 @@ function continuouslyPaintFrame() {
   canvas.height = videoHeight;
   setInterval(() => {
     ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+    modifyPaintedFrame(videoWidth, videoHeight);
   }, refreshTime);
 }
 
--- notes/19-Webcam.Fun/Notes.md
+++ notes/19-Webcam.Fun/Notes.md
@@ -26,6 +26,17 @@ How do we paint the last frame **continuously** in the canvas?
 - 3. What size should the canvas be?
   - The size the video captured: [height](https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/videoHeight) and [width](https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/videoWidth)
 
+### Task 3
+
+To modify the actual frame, we need to read a frame as an image.
+
+The image is actually a huge array of `rgba` values (4 values) of each individual pixel.
+![Uint8ClampedArray](Uint8ClampedArray.png)
+
+Each value is a bit ranging from 0 to 255.
+
+From there, you can play with the value to obtain which ever filter you want.
+
 ### Task 4
 
 To get the image as jpeg, we can use [toDataURL](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL) of the canvas element.
