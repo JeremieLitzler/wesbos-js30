19d554a3177ace2e630560a783b43c595313b35f
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-01-14 07:08:58 +0100

    exercice 22

---
+++ exercices/22-Follow.Along.Link.Highlighter/followme.js
@@ -0,0 +1,24 @@
+const links = document.querySelectorAll('a');
+
+const bubble = document.createElement('span');
+bubble.classList.add('highlight');
+document.body.appendChild(bubble);
+
+function highlightLink(event) {
+  console.log(event);
+  //we want to find where is the link
+  const bounds = this.getBoundingClientRect();
+  console.log(bounds);
+
+  //what is the size of the link?
+  //bounds.height and bounds.width give it
+  bubble.style.width = `${bounds.width}px`;
+  bubble.style.height = `${bounds.height}px`;
+  bubble.style.padding = '1em';
+  console.dir(window);
+  const translateX = bounds.left;
+  const translateY = bounds.top - 4; //4px to center the bubble
+  bubble.style.transform = `translate(${translateX}px, ${translateY}px)`;
+}
+
+links.forEach((a) => a.addEventListener('mouseover', highlightLink));
---
+++ notes/22-Follow.Along.Link.Highlighter/Notes.md
@@ -0,0 +1,16 @@
+# Follow Along Link Highlighter
+
+[< Back to table of content](../README.md) |
+[View previous exercice's notes >](../21-Geolocation/Notes.md) |
+[View next exercice's notes >](../23-Speech.Synthesis/Notes.md)
+
+## Instructions
+
+Replicate the menu interactivity of [Stripe menu](https://stripe.com/) when hovering the main menu items.
+
+Per Wes's example, we will want all links in the sample page to work with a highlight "bubble" that transition from the previously highlighted link to the new one.
+
+The bubble is a dynamic element that is positionned from the previous hovered link to the next.
+By default is not visible.
+
+The sub-menu part will be dealt in [exercice 26](../26-Stripe.Follow.Along.Nav/Notes.md)
